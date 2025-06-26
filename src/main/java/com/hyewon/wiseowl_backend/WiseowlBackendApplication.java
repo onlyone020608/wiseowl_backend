547a445a8ab161d8ae10eefbcb39b5f7d185a9ee
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-06-26 17:09:26 +0900

    chore(init): initialize Spring Boot project

---
+++ src/main/java/com/hyewon/wiseowl_backend/WiseowlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hyewon.wiseowl_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiseowlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiseowlBackendApplication.class, args);
+	}
+
+}
