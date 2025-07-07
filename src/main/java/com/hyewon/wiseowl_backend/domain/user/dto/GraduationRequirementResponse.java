211ca92d3d73c8a7b3080b5ecfa8ac6370f7e2e3
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-07-08 08:07:48 +0900

    refactor(user): update GraduationRequirementResponse to include userRequirementStatusId

--- src/main/java/com/hyewon/wiseowl_backend/domain/user/dto/GraduationRequirementResponse.java
+++ src/main/java/com/hyewon/wiseowl_backend/domain/user/dto/GraduationRequirementResponse.java
@@ -3,12 +3,13 @@ package com.hyewon.wiseowl_backend.domain.user.dto;
 import com.hyewon.wiseowl_backend.domain.requirement.entity.Requirement;
 import com.hyewon.wiseowl_backend.domain.user.entity.UserRequirementStatus;
 
-public record GraduationRequirementResponse( String name,
+public record GraduationRequirementResponse( Long userRequirementStatusId, String name,
                                              String description,
                                              boolean fulfilled){
     public static GraduationRequirementResponse from(UserRequirementStatus status) {
         Requirement r = status.getMajorRequirement().getRequirement();
         return new GraduationRequirementResponse(
+                status.getId(),
                 r.getName(),
                 status.getMajorRequirement().getDescription(),
                 status.isFulfilled()
