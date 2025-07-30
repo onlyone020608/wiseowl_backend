cf9185d34e4b6f9df052507dae85d4f5ba0d873f
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-07-30 13:47:50 +0900

    test(user-subscription): add unit tests for user subscription retrieval

---
+++ src/test/java/com/hyewon/wiseowl_backend/domain/UserSubscriptionServiceTest.java
@@ -0,0 +1,64 @@
+package com.hyewon.wiseowl_backend.domain;
+
+import com.hyewon.wiseowl_backend.domain.user.entity.SubscriptionType;
+import com.hyewon.wiseowl_backend.domain.user.entity.User;
+import com.hyewon.wiseowl_backend.domain.user.entity.UserSubscription;
+import com.hyewon.wiseowl_backend.domain.user.repository.UserSubscriptionRepository;
+import com.hyewon.wiseowl_backend.domain.user.service.UserSubscriptionService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class UserSubscriptionServiceTest {
+    @InjectMocks
+    UserSubscriptionService userSubscriptionService;
+
+    @Mock
+    UserSubscriptionRepository userSubscriptionRepository;
+
+    private UserSubscription userSubscription;
+    private User user;
+    @BeforeEach
+    void setUp() {
+        user = User.builder()
+                .id(1L)
+                .build();
+
+        userSubscription = UserSubscription.builder()
+                .user(user)
+                .targetId(2L)
+                .type(SubscriptionType.MAJOR)
+                .build();
+
+    }
+    @Test
+    @DisplayName("getSubscriptions - should return user's subscription list")
+    void getSubscriptions_shouldSucceed() {
+        // given
+        Long userId = 1L;
+        given(userSubscriptionRepository.findAllByUserId(userId)).willReturn(
+                List.of(userSubscription)
+        );
+
+        // when
+        List<UserSubscription> subscriptions = userSubscriptionService.getSubscriptions(userId);
+
+        // then
+        assertThat(subscriptions).hasSize(1);
+        assertThat(userSubscription).isEqualTo(subscriptions.get(0));
+
+
+    }
+}
+
