cdd77dcb13f153aa5fbd039a0124cf29e745e3d3
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-07-31 15:28:04 +0900

    test(required-major-course-query): add unit tests for required major course retrieval

---
+++ src/test/java/com/hyewon/wiseowl_backend/domain/RequiredMajorCourseQueryServiceTest.java
@@ -0,0 +1,93 @@
+package com.hyewon.wiseowl_backend.domain;
+
+import com.hyewon.wiseowl_backend.domain.course.entity.Major;
+import com.hyewon.wiseowl_backend.domain.requirement.entity.MajorType;
+import com.hyewon.wiseowl_backend.domain.requirement.entity.RequiredMajorCourse;
+import com.hyewon.wiseowl_backend.domain.requirement.repository.RequiredMajorCourseRepository;
+import com.hyewon.wiseowl_backend.domain.requirement.service.RequiredMajorCourseQueryService;
+import com.hyewon.wiseowl_backend.global.exception.RequiredMajorCourseNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class RequiredMajorCourseQueryServiceTest {
+    @InjectMocks
+    RequiredMajorCourseQueryService requiredMajorCourseQueryService;
+
+    @Mock
+    RequiredMajorCourseRepository reqMajorCourseRepository;
+
+    private Major major;
+
+    private RequiredMajorCourse requiredMajorCourse;
+
+    @BeforeEach
+    void setUp() {
+        major = Major.builder()
+                .id(1L)
+                .build();
+        requiredMajorCourse = RequiredMajorCourse.builder()
+                .id(2L)
+                .major(major)
+                .majorType(MajorType.PRIMARY)
+                .build();
+
+    }
+
+    @Test
+    @DisplayName("getApplicableMajorCourses - should return applicable required major course")
+    void getApplicableMajorCourses_shouldSucceed() {
+
+        // given
+        given(reqMajorCourseRepository.findApplicableMajorCourses(1L, MajorType.PRIMARY, 2021)).willReturn(
+                List.of(requiredMajorCourse)
+        );
+
+        // when
+        List<RequiredMajorCourse> applicableMajorCourses = requiredMajorCourseQueryService.getApplicableMajorCourses(1L, MajorType.PRIMARY, 2021);
+
+        // then
+        assertThat(applicableMajorCourses).hasSize(1);
+        assertThat(applicableMajorCourses.get(0)).isEqualTo(requiredMajorCourse);
+    }
+
+    @Test
+    @DisplayName("getRequiredMajorCourse - should return required major course")
+    void getRequiredMajorCourse_shouldSucceed() {
+
+        // given
+        given(reqMajorCourseRepository.findById(2L)).willReturn(
+                Optional.of(requiredMajorCourse));
+
+        // when
+        RequiredMajorCourse result = requiredMajorCourseQueryService.getRequiredMajorCourse(2L);
+
+        // then
+        assertThat(result).isEqualTo(requiredMajorCourse);
+    }
+
+    @Test
+    @DisplayName("getRequiredMajorCourse - should throw RequiredMajorCourseNotFoundException when required major course does not exist")
+    void getRequiredMajorCourse_shouldThrowException_whenRequiredMajorCourseNotFound() {
+
+        // given
+        given(reqMajorCourseRepository.findById(999L)).willReturn(
+                Optional.empty());
+
+        // when & then
+        assertThrows(RequiredMajorCourseNotFoundException.class,
+                () ->  requiredMajorCourseQueryService.getRequiredMajorCourse(999L));
+    }
+}
