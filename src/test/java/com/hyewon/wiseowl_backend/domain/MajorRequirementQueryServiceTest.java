1e120445562c5be8b92582f6f215774cc078266f
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-07-31 14:37:41 +0900

    test(major-requirement-query): add unit tests for applicable major requirement retrieval

---
+++ src/test/java/com/hyewon/wiseowl_backend/domain/MajorRequirementQueryServiceTest.java
@@ -0,0 +1,63 @@
+package com.hyewon.wiseowl_backend.domain;
+
+import com.hyewon.wiseowl_backend.domain.course.entity.Major;
+import com.hyewon.wiseowl_backend.domain.requirement.entity.MajorRequirement;
+import com.hyewon.wiseowl_backend.domain.requirement.entity.MajorType;
+import com.hyewon.wiseowl_backend.domain.requirement.repository.MajorRequirementRepository;
+import com.hyewon.wiseowl_backend.domain.requirement.service.MajorRequirementQueryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class MajorRequirementQueryServiceTest {
+
+    @InjectMocks
+    MajorRequirementQueryService majorRequirementQueryService;
+
+    @Mock
+    MajorRequirementRepository majorRequirementRepository;
+
+    private Major major;
+    private MajorRequirement majorRequirement;
+
+    @BeforeEach
+    void setUp() {
+        major = Major.builder()
+                .id(1L)
+                .build();
+       majorRequirement = MajorRequirement.builder()
+               .majorType(MajorType.PRIMARY)
+               .major(major)
+               .build();
+
+    }
+
+    @Test
+    @DisplayName("getMajorName - should return major name")
+    void getApplicableRequirements_shouldSucceed() {
+        // given
+        given(majorRequirementRepository.findApplicable(1L, MajorType.PRIMARY, 2021)).willReturn(
+                List.of(majorRequirement)
+               );
+
+        // when
+        List<MajorRequirement> applicableRequirements = majorRequirementQueryService.getApplicableRequirements(1L, MajorType.PRIMARY, 2021);
+
+        // then
+        assertThat(applicableRequirements).hasSize(1);
+        assertThat(applicableRequirements.get(0)).isEqualTo(majorRequirement);
+
+    }
+
+}
