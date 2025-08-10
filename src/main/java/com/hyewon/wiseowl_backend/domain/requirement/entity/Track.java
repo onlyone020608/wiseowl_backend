be7d25f373c60852e0c27476d699a556024cae48
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-08-10 16:28:57 +0900

    style(track): reorder enum constants for readability

--- src/main/java/com/hyewon/wiseowl_backend/domain/requirement/entity/Track.java
+++ src/main/java/com/hyewon/wiseowl_backend/domain/requirement/entity/Track.java
@@ -3,6 +3,6 @@ package com.hyewon.wiseowl_backend.domain.requirement.entity;
 public enum Track {
     PRIMARY_WITH_DOUBLE, // 본전공 + 이중
     PRIMARY_WITH_MINOR,  // 본전공 + 부전
-    INTENSIVE_SOLO,      // 전공심화
-    INTENSIVE_WITH_MINOR  // 전공심화 + 부전공
+    INTENSIVE_WITH_MINOR,  // 전공심화 + 부전공
+    INTENSIVE_SOLO        // 전공심화
 }
