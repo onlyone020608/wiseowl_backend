860541bcd18a1a99a6183d78da90f57470a0d895
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-08-13 07:56:16 +0900

    refactor(requirement): replace implicit join with explicit join in findApplicableLiberalCategories

--- src/main/java/com/hyewon/wiseowl_backend/domain/requirement/repository/RequiredLiberalCategoryQueryRepositoryImpl.java
+++ src/main/java/com/hyewon/wiseowl_backend/domain/requirement/repository/RequiredLiberalCategoryQueryRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.hyewon.wiseowl_backend.domain.requirement.repository;
 
+import com.hyewon.wiseowl_backend.domain.course.entity.QCollege;
 import com.hyewon.wiseowl_backend.domain.requirement.entity.QRequiredLiberalCategoryByCollege;
 import com.hyewon.wiseowl_backend.domain.requirement.entity.RequiredLiberalCategoryByCollege;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -14,10 +15,13 @@ public class RequiredLiberalCategoryQueryRepositoryImpl implements RequiredLiber
     @Override
     public List<RequiredLiberalCategoryByCollege> findApplicableLiberalCategories(Long collegeId, int entranceYear) {
         QRequiredLiberalCategoryByCollege rlc = QRequiredLiberalCategoryByCollege.requiredLiberalCategoryByCollege;
+        QCollege college = QCollege.college;
+
         return query.select(rlc)
                 .from(rlc)
+                .join(rlc.college, college)
                 .where(
-                        rlc.college.id.eq(collegeId),
+                        college.id.eq(collegeId),
                         rlc.appliesFromYear.coalesce(0).loe(entranceYear),
                         rlc.appliesToYear.coalesce(Integer.MAX_VALUE).goe(entranceYear)
                 )
