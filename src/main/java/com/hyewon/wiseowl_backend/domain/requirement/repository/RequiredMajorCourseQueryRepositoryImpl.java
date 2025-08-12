1cfccf687fc73f6b35c76a4622027cc20c375b95
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-08-13 07:58:50 +0900

    refactor(requirement): replace implicit join with explicit join in findApplicableMajorCourses

--- src/main/java/com/hyewon/wiseowl_backend/domain/requirement/repository/RequiredMajorCourseQueryRepositoryImpl.java
+++ src/main/java/com/hyewon/wiseowl_backend/domain/requirement/repository/RequiredMajorCourseQueryRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.hyewon.wiseowl_backend.domain.requirement.repository;
 
+import com.hyewon.wiseowl_backend.domain.course.entity.QMajor;
 import com.hyewon.wiseowl_backend.domain.requirement.entity.MajorType;
 import com.hyewon.wiseowl_backend.domain.requirement.entity.QRequiredMajorCourse;
 import com.hyewon.wiseowl_backend.domain.requirement.entity.RequiredMajorCourse;
@@ -16,11 +17,13 @@ public class RequiredMajorCourseQueryRepositoryImpl implements RequiredMajorCour
     @Override
     public List<RequiredMajorCourse> findApplicableMajorCourses(Long majorId, MajorType majorType, Integer entranceYear) {
         QRequiredMajorCourse rmc = QRequiredMajorCourse.requiredMajorCourse;
+        QMajor major = QMajor.major;
 
         return query.select(rmc)
                 .from(rmc)
+                .join(rmc.major, major)
                 .where(
-                        rmc.major.id.eq(majorId),
+                        major.id.eq(majorId),
                         rmc.majorType.eq(majorType),
                         rmc.appliesFromYear.coalesce(0).loe(entranceYear),
                         rmc.appliesToYear.coalesce(Integer.MAX_VALUE).goe(entranceYear)
