5cd52a8c6b2a45655cb63f298f7b2bcc43f4bd06
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-08-16 09:25:23 +0900

    refactor(notice): unify NoticeType into SubscriptionType and remove duplicate enum

--- src/main/java/com/hyewon/wiseowl_backend/domain/notice/entity/Notice.java
+++ src/main/java/com/hyewon/wiseowl_backend/domain/notice/entity/Notice.java
@@ -1,5 +1,6 @@
 package com.hyewon.wiseowl_backend.domain.notice.entity;
 
+import com.hyewon.wiseowl_backend.domain.user.entity.SubscriptionType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
@@ -25,7 +26,7 @@ public class Notice {
     private LocalDate postedAt;
 
     @Enumerated(EnumType.STRING)
-    private NoticeType noticeType;
+    private SubscriptionType type;
 
     private Long sourceId;
 
--- src/main/java/com/hyewon/wiseowl_backend/domain/notice/entity/NoticeType.java
+++
@@ -1,6 +0,0 @@
-package com.hyewon.wiseowl_backend.domain.notice.entity;
-
-public enum NoticeType {
-    MAJOR,
-    ORGANIZATION
-}
