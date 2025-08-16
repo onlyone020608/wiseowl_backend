cfd6f16f675d0bbee35bbaefd4fcc566719d2b59
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-08-16 10:16:51 +0900

    refactor(facility): simplify getAllFacilities by using Collectors for grouping and mapping

--- src/main/java/com/hyewon/wiseowl_backend/domain/facility/service/FacilityService.java
+++ src/main/java/com/hyewon/wiseowl_backend/domain/facility/service/FacilityService.java
@@ -1,6 +1,5 @@
 package com.hyewon.wiseowl_backend.domain.facility.service;
 
-import com.hyewon.wiseowl_backend.domain.course.entity.Building;
 import com.hyewon.wiseowl_backend.domain.facility.dto.BuildingFacilityResponse;
 import com.hyewon.wiseowl_backend.domain.facility.dto.FacilityResponse;
 import com.hyewon.wiseowl_backend.domain.facility.entity.Facility;
@@ -11,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,25 +20,20 @@ public class FacilityService {
 
     @Transactional(readOnly = true)
     public List<BuildingFacilityResponse> getAllFacilities() {
-        List<Facility> all = facilityRepository.findAll();
-        if (all.isEmpty()) {
+        List<Facility> facilities = facilityRepository.findAll();
+        if (facilities.isEmpty()) {
             throw new FacilityNotFoundException();
         }
-        Map<Building, List<Facility>> grouped = all.stream()
-                .collect(Collectors.groupingBy(Facility::getBuilding));
 
-        return grouped.entrySet().stream().map(
-                entry -> {
-                    Building building = entry.getKey();
-                    List<FacilityResponse> facilities = entry.getValue().stream().map(
-                            FacilityResponse::from
-                    ).toList();
-                    return new BuildingFacilityResponse(
-                            building.getBuildingNumber(),
-                            building.getName(),
-                            facilities
-                    );
-                }
-        ).toList();
+        return facilities.stream()
+                .collect(Collectors.groupingBy(Facility::getBuilding,
+                        Collectors.mapping(FacilityResponse::from, Collectors.toList())))
+                .entrySet().stream()
+                .map(entry -> new BuildingFacilityResponse(
+                        entry.getKey().getBuildingNumber(),
+                        entry.getKey().getName(),
+                        entry.getValue()
+                        ))
+                .toList();
     }
 }
