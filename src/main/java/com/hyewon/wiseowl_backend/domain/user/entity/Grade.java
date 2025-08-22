7a99273638ef55316d424e8cbfb3cd10129af679
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-08-22 10:57:54 +0900

    feat(user): add @JsonProperty mapping for Grade values

--- src/main/java/com/hyewon/wiseowl_backend/domain/user/entity/Grade.java
+++ src/main/java/com/hyewon/wiseowl_backend/domain/user/entity/Grade.java
@@ -1,13 +1,17 @@
 package com.hyewon.wiseowl_backend.domain.user.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public enum Grade {
+    @JsonProperty("A+")
     A_PLUS(4.5),
     A(4.0),
+    @JsonProperty("B+")
     B_PLUS(3.5),
     B(3.0),
+    @JsonProperty("C+")
     C_PLUS(2.5),
     C(2.0),
     D(1.0),
