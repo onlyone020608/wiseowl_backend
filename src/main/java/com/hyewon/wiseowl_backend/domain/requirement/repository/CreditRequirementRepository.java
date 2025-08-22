3bde2c880c53705577843dd02789d6f166b6daf4
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-08-22 10:52:19 +0900

    fix(repository): correct primary key type from Integer to Long in CreditRequirementRepository

--- src/main/java/com/hyewon/wiseowl_backend/domain/requirement/repository/CreditRequirementRepository.java
+++ src/main/java/com/hyewon/wiseowl_backend/domain/requirement/repository/CreditRequirementRepository.java
@@ -3,5 +3,5 @@ package com.hyewon.wiseowl_backend.domain.requirement.repository;
 import com.hyewon.wiseowl_backend.domain.requirement.entity.CreditRequirement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CreditRequirementRepository extends JpaRepository<CreditRequirement, Integer>, CreditRequirementQueryRepository {
+public interface CreditRequirementRepository extends JpaRepository<CreditRequirement, Long>, CreditRequirementQueryRepository {
 }
