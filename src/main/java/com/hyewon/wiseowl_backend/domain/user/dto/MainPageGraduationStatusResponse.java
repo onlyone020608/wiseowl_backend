85a1496470a9ca994efdd6a71ae6cc9ff7150d49
onlyone020608 <onlyone0608@hufs.ac.kr>
2025-08-25 11:08:02 +0900

    refactor(dto): rename userName to username in MainPageGraduationStatusResponse

--- src/main/java/com/hyewon/wiseowl_backend/domain/user/dto/MainPageGraduationStatusResponse.java
+++ src/main/java/com/hyewon/wiseowl_backend/domain/user/dto/MainPageGraduationStatusResponse.java
@@ -3,7 +3,7 @@ package com.hyewon.wiseowl_backend.domain.user.dto;
 import java.util.List;
 
 public record MainPageGraduationStatusResponse(
-        String userName,
+        String username,
         List<RequirementStatusByMajor> requirementStatuses
 
 ) {
